c2424ef6dd13aa882ecf5412e7a748b7243dabad
AzKurban2 <azkurban@gmail.com>
2026-04-15 23:56:18 +0300

    Function enhanced: tune.rf.

--- r/support-functions/models-helper.R
+++ r/support-functions/models-helper.R
@@ -28,7 +28,21 @@ tune.rf <- function(x,
                     y.test,
                     mtry = NA, 
                     n.tree = 200,
+                    cache_root = NULL,
                     cache_file = NULL){
+  
+  local_root <- NULL
+  local_cache.path <- NULL
+  
+  if(!is.null(cache_root)){
+    local_root <- file.path(cache_root, "tune.rf.cache")
+    
+    cache_file <- file.path(cache_root, cache_file)
+    
+    if(!dir.exists(local_root))
+      dir.create(local_root)
+  }
+
   cl <- makeCluster(N_pcCores)
   registerDoParallel(cl)
   
@@ -37,29 +51,65 @@ tune.rf <- function(x,
   if (!is.null(cache_file) && file.exists(cache_file)) {
     put_log("Function `tune.rf`:
 Loading Model Fit Data from cache file: 
-%1", cache_file.path)
+%1", cache_file)
     
-    load(cache_file.path)
+    load(cache_file)
     put_log("Function `tune.rf`:
 Train Data list has been loaded from cache.")
     put_end_date(start)
     
   } else {
-    
+
     mtry.tuned_result <- lapply(mtry, function(mtry.val){
-      put_log("Function `tune.rf`:
-Tuning `RF` model for `mtry = %1`...", mtry.val)
       start <- put_start_date()
       
-      if (is.na(mtry.val)) {
-        fit <-randomForest(x, 
-                           y,  
-                           ntree = n.tree)
+      if (!is.null(local_root)) {
+        m <- ifelse(is.na(mtry.val), "default", mtry.val)
+        local_cache <- str.build("rf.fit.ntree%1.mtry%2.RData", n.tree, m)
+        local_cache.path <- file.path(local_root, local_cache)
+      }
+      
+      if (!is.null(local_cache.path) && file.exists(local_cache.path)) {
+        put_log("Function `tune.rf`:
+Loading RF Tuning Fit Data from local cache file: 
+%1", local_cache.path)
+        
+        load(local_cache.path)
+        put_log("Function `tune.rf`:
+RF Tuning Fit Data has been loaded from cache.")
+        put_end_date(start)
+        
       } else {
-        fit <-randomForest(x, 
-                           y,  
-                           mtry = mtry.val, 
-                           ntree = n.tree)
+        
+
+        if (is.na(mtry.val)) {
+          put_log("Function `tune.rf`:
+  Tuning `RF` model for the default value of `mtry`...")
+  
+          fit <-randomForest(x, 
+                             y,  
+                             ntree = n.tree)
+        } else {
+          put_log("Function `tune.rf`:
+  Tuning `RF` model for `mtry = %1`...", mtry.val)
+          
+          fit <-randomForest(x, 
+                             y,  
+                             mtry = mtry.val, 
+                             ntree = n.tree)
+        }
+        
+        if (!is.null(local_cache.path)){
+          
+          put_log("Function `tune.rf`:
+Caching the model tuning fit result...")
+
+          save(fit, file = local_cache.path)
+
+          put_log("Function `tune.rf`:
+The model tuning fit result has been cached to file:
+%1.", local_cache.path)
+        }
       }
       
       plot(fit)
@@ -98,7 +148,8 @@ The accuracy value is %1", acc)
       put_end_date(start)
       # Time difference of ??? mins
       
-      list(mtry=mtry.val, 
+      list(in.mtry=mtry.val,
+           fit = fit,
            predictions = y_hat,
            err.rate = fit$err.rate,
            accuracy = acc)
@@ -124,7 +175,7 @@ Saving the model tuning result...")
            file = cache_file)
       put_log("Function `tune.rf`:
 The Pre-train fit result has been saved to the cache file:
-%1.", cache_file.path)
+%1.", cache_file)
     }
   }
   
