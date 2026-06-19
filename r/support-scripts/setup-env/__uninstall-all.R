215b3a03c70a6c1e415380240fa2ecb487b57083
AzKurban2 <azkurban@gmail.com>
2026-06-19 10:14:19 +0300

    Clean-up environment implemented.

---
+++ r/support-scripts/setup-env/__uninstall-all.R
@@ -0,0 +1,12 @@
+detach("package:keras", unload = TRUE)
+detach("package:kerastuneR", unload = TRUE)
+detach("package:autokeras", unload = TRUE)
+detach("package:keras3", unload = TRUE)
+detach("package:tensorflow", unload = TRUE)
+detach("package:reticulate", unload = TRUE)
+# detach("package:", unload = TRUE)
+# detach("package:", unload = TRUE)
+# detach("package:", unload = TRUE)
+# 
+
+
