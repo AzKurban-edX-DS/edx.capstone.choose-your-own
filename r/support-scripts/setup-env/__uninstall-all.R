22d3dfa5512a9d6d793651c518481029c47454d4
AzKurban2 <azkurban@gmail.com>
2026-06-19 10:44:45 +0300

    Setup environment feature: Uninstall All script refined.

--- r/support-scripts/setup-env/__uninstall-all.R
+++ r/support-scripts/setup-env/__uninstall-all.R
@@ -1,9 +1,30 @@
-detach("package:keras", unload = TRUE)
-detach("package:kerastuneR", unload = TRUE)
-detach("package:autokeras", unload = TRUE)
-detach("package:keras3", unload = TRUE)
-detach("package:tensorflow", unload = TRUE)
-detach("package:reticulate", unload = TRUE)
+if(require(keras)) {
+  detach("package:keras", unload = TRUE)
+}
+
+if(require(kerastuneR)) {
+  detach("package:kerastuneR", unload = TRUE)
+}
+
+if(require(autokeras)) {
+  detach("package:autokeras", unload = TRUE)
+}
+
+if(require(keras3)) {
+  detach("package:keras3", unload = TRUE)
+}
+
+if(require(tensorflow)) {
+  detach("package:tensorflow", unload = TRUE)
+}
+
+if(require(reticulate)) {
+  library(reticulate)
+  # print("Hello reticulate!")
+  reticulate::miniconda_uninstall()
+  detach("package:reticulate", unload = TRUE)
+}
+
 # detach("package:", unload = TRUE)
 
 .rs.restartR()
