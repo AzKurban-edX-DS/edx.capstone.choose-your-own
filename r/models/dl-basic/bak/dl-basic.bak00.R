5621938f598ec1f60c2b637c9dd3ac7e8b9a29a7
AzKurban2 <azkurban@gmail.com>
2026-07-24 03:25:18 +0300

    DL Basic script: Reserve copy added.

---
+++ r/models/dl-basic/bak/dl-basic.bak00.R
@@ -0,0 +1,503 @@
+#%%%%%%%%%%%%%%%%%%%%%
+# BDL MCC  Model
+#%%%%%%%%%%%%%%%%%%%%%
+
+# Basic Deep Learning Multiclass Classifier (BDL MCC)  Model
+
+# References:
+# MNIST Handwritten Digit Recognition in Keras
+# https://nextjournal.com/gkoehler/digit-recognition-with-keras
+# ref.bib: DL_R3_E2-S7.3
+
+## Prepare Input Datasets -----------------------------------------------------
+open_logfile(".dl.basic-model.prepare-ds")
+stopifnot(file.exists(my_emnist.0.1split.file_path))
+
+open_logfile(".rf.load-split.10%train.balanced_sample")
+start <- put_start_date()
+
+### Loading Split Flattened Dataset allocated 10% for the Train Set ------------
+
+put_log("Loading the Split Flattened Dataset from the backup file...")
+
+ds <- load_datasets(my_emnist.0.1split.file_path)
+
+put_log("The Split Flattened Dataset has been loaded from the folowing file:
+%1", my_emnist.0.1split.file_path)
+
+put_log("The Split Flattened Dataset has the follwoing structure:
+%1", capture.output(str(ds)))
+
+x_train <- ds$train$x
+shape(x_train)
+
+put_log("The Train set is balanced with respect to the set of classes:
+%1", capture.output(print(ds$train$class_groups$groupByClass, n = N.classes)))
+{
+  # A tibble: 39 × 2
+  #    classID     n
+  #    <fct>   <int>
+  #  1 #         425
+  #  2 $         425
+  #  3 &         425
+  #  4 @         425
+  #  5 0         425
+  #  6 1         425
+  #  7 2         425
+  #  8 3         425
+  #  9 4         425
+  # 10 5         425
+  # 11 6         425
+  # 12 7         425
+  # 13 8         425
+  # 14 9         425
+  # 15 A         425
+  # 16 B         425
+  # 17 C         425
+  # 18 D         425
+  # 19 E         425
+  # 20 F         425
+  # 21 G         425
+  # 22 H         425
+  # 23 I         425
+  # 24 J         425
+  # 25 K         425
+  # 26 L         425
+  # 27 M         425
+  # 28 N         425
+  # 29 P         425
+  # 30 Q         425
+  # 31 R         425
+  # 32 S         425
+  # 33 T         425
+  # 34 U         425
+  # 35 V         425
+  # 36 W         425
+  # 37 X         425
+  # 38 Y         425
+  # 39 Z         425  
+  invisible(NULL)
+}
+
+y_train <- ds$train$class_groups$classID
+
+
+stopifnot(sum(as.character(y_train) != rownames(x_train)) == 0)
+stopifnot(nrow(x_train) == length(y_train))
+
+x_test <- ds$test$x
+
+put_log("The Test set is balanced with respect to the set of classes:
+%1", capture.output(print(ds$test$class_groups$groupByClass, n = N.classes)))
+{
+  # # A tibble: 39 × 2
+  #    classID     n
+  #    <fct>   <int>
+  #  1 #        3834
+  #  2 $        3834
+  #  3 &        3834
+  #  4 @        3834
+  #  5 0        3834
+  #  6 1        3834
+  #  7 2        3834
+  #  8 3        3834
+  #  9 4        3834
+  # 10 5        3834
+  # 11 6        3834
+  # 12 7        3834
+  # 13 8        3834
+  # 14 9        3834
+  # 15 A        3834
+  # 16 B        3834
+  # 17 C        3834
+  # 18 D        3834
+  # 19 E        3834
+  # 20 F        3834
+  # 21 G        3834
+  # 22 H        3834
+  # 23 I        3834
+  # 24 J        3834
+  # 25 K        3834
+  # 26 L        3834
+  # 27 M        3834
+  # 28 N        3834
+  # 29 P        3834
+  # 30 Q        3834
+  # 31 R        3834
+  # 32 S        3834
+  # 33 T        3834
+  # 34 U        3834
+  # 35 V        3834
+  # 36 W        3834
+  # 37 X        3834
+  # 38 Y        3834
+  # 39 Z        3834
+  invisible(NULL)
+}
+
+y_test <- ds$test$class_groups$classID
+
+stopifnot(sum(as.character(y_test) != rownames(x_test)) == 0)
+stopifnot(nrow(x_test) == length(y_test))
+rm(ds)
+
+### Converting labels factor to categorical -----------------------------------
+# Reference: 
+#> Deep Learning with R and Keras: Build a Handwritten Digit Classifier in 10 Minutes
+# https://www.appsilon.com/post/r-keras-mnist#:~:text=do%20that%20next.-,Model%20Training,function%20to%20train%20the%20model.
+# https://www.r-bloggers.com/2021/02/deep-learning-with-r-and-keras-build-a-handwritten-digit-classifier-in-10-minutes/
+
+
+y_train.cat <- to_categorical(y_train)
+colnames(y_train.cat) <- Y.Labels
+dim(y_train.cat)
+str(y_train.cat)
+head(y_train.cat)
+# max(y_train.cat)
+
+y_test.cat <- to_categorical(y_test)
+colnames(y_test.cat) <- Y.Labels
+dim(y_test.cat)
+str(y_test.cat)
+head(y_test.cat)
+
+### Analyzing Image Data --------------------------------------------------------
+max_img_pixels <- max(rowSums(x_train))
+put_log("The maximum number of pixels in the Train Set images is as follows: %1",
+        max_img_pixels)
+#> 593
+
+# References:
+# https://community.deeplearning.ai/t/number-of-the-hidden-units-in-hidden-layers/24916
+# https://milvus.io/ai-quick-reference/how-do-you-decide-the-number-of-neurons-per-layer
+# https://tracyrenee61.medium.com/how-do-you-calculate-the-correct-number-of-neurons-to-place-in-the-hidden-layers-of-a-neural-19cbaf68452c
+# https://arxiv.org/pdf/1707.09725#page=11
+# https://app.speechify.com/item/bde53a50-bd0e-4328-8273-27caf5174899?type=WEB
+# https://www.kaggle.com/discussions/general/321114
+# https://ijettjournal.org/assets/volume-3/issue-6/IJETT-V3I6P206.pdf
+# https://www.heatonresearch.com/2017/06/01/hidden-layers.html [JH_NHL]
+
+n.hl.units <- ceiling(28*28/3*2 + 39)
+# 562
+
+put_log("For the couple of hidden layers of our first deep learning model, 
+we will choose %1 neurons, which meets the value suggested by the rule-of-thumb methods 
+(outlined in the article 'Heaton Research: The Number of Hidden Layers' [JH_NHL2017-06-01]), 
+and also does not exceed the maximum number of black pixels (%2) making up every (pre-processed) image 
+contained in the training set.",
+        n.hl.units,
+        max_img_pixels)
+
+log_close()
+
+## Init DL Basic Model Paths ---------------------------------------------------
+
+open_logfile(".dl.basic-model.build")
+
+dl.basic.dir_path <- file.path(dl.keras3.dir, "dl.basic")
+
+if(!dir.exists(dl.basic.dir_path))
+  dir.create(dl.basic.dir_path)
+
+dl.basic.tuning.dir <- file.path(dl.basic.dir_path,
+                                            "tuning")
+if(!dir.exists(dl.basic.tuning.dir))
+  dir.create(dl.basic.tuning.dir)
+
+dl.basic.layers_dynamic.dir <- file.path(dl.basic.tuning.dir, "layers-dynamic")
+
+if(!dir.exists(dl.basic.layers_dynamic.dir))
+  dir.create(dl.basic.layers_dynamic.dir)
+
+dl.basic.keras_tuner.dir <- file.path(dl.basic.tuning.dir, "keras-tuner")
+
+if(!dir.exists(dl.basic.keras_tuner.dir))
+  dir.create(dl.basic.keras_tuner.dir)
+
+dl.basic.checkpoints.dir <- file.path(dl.basic.dir_path,
+                                            "checkpoints")
+if(!dir.exists(dl.basic.checkpoints.dir))
+  dir.create(dl.basic.checkpoints.dir)
+
+dl.basic.checkpoint.file_path <- 
+  file.path(dl.basic.checkpoints.dir, 
+            "dl.basic.{epoch:02d}-{val_loss:.2f}.keras")
+
+dl.basic.model.file_path <- file.path(dl.basic.dir_path, 
+                             "dl.basic.pre-trained.model.keras")
+
+dl.basic.model.train_history.file_path <- file.path(dl.basic.dir_path, 
+                             "dl.basic.model.train_history.bak.rds")
+
+## Building Basic DL MCC Model -------------------------------------------------
+
+
+n.input_shape <- ncol(x_train)
+# 784
+
+if(file.exists(dl.basic.model.file_path)) {
+  put_log("Loading pre-trained BDL MCC Model...")
+  
+  dl.basic.model <- keras3::load_model(dl.basic.model.file_path)
+  
+  put_log("The BDL MCC Model has been loaded from the backup file:
+%1", dl.basic.model.file_path)
+  
+  if(file.exists(dl.basic.model.train_history.file_path)){
+    put_log("Loading the BDL MCC Model Train History...")
+    
+    dl.basic.train.flatten_history <- readRDS(dl.basic.model.train_history.file_path)
+    
+    put_log("The BDL MCC Model has been loaded from the backup file:
+%1", dl.basic.model.train_history.file_path)
+  } else {
+    warning("The BDL MCC Model backup does not exist:
+", dl.basic.model.train_history.file_path)
+  }
+} else {
+  ### Defining & Compiling the Basic DL MCC Model ******************************
+  
+  n.input_shape <- ncol(x_train)
+  # 784
+  
+  dl.basic.inputs <- layer_input(shape = c(n.input_shape))
+  
+  dl.basic.outputs <- dl.basic.inputs |>
+    layer_dense(units = n.hl.units, activation = "relu") |>
+    layer_dropout(rate = 0.25) |> 
+    layer_dense(units = N.classes, activation = "softmax")
+  
+  
+  dl.basic.model <- keras_model(dl.basic.inputs, dl.basic.outputs)
+  # dl.basic.model
+  
+  dl.basic.model |> compile(
+    loss = "categorical_crossentropy",
+    optimizer = keras3::optimizer_adamax(0.001),
+    metrics = "accuracy"
+  )
+  
+  summary(dl.basic.model)
+  
+  ### Training the Basic DL MCC Model ******************************************
+  
+  dl.basic.callbacks <- list(
+    callback_early_stopping(patience = 3, monitor = 'val_accuracy'),
+    callback_model_checkpoint(filepath = dl.basic.checkpoint.file_path,
+                              monitor = "val_loss",
+                              save_best_only = TRUE,
+                              verbose = 1)
+  )
+  
+  
+
+  put_log("Training the BDL MCC Model...")
+  start <- put_start_date()
+  
+  dl.basic.train.flatten_history <- dl.basic.model |> 
+    fit(x_train, 
+        y_train.cat, 
+        epochs = 100, 
+        # batch_size = 128, 
+        callbacks = dl.basic.callbacks,
+        validation_split = 0.2
+        )
+
+  put_log("Saving pre-trained BDL MCC Model...")
+  keras3::save_model(dl.basic.model,
+             filepath = dl.basic.model.file_path,
+             overwrite = FALSE)
+  
+  put_log("The BDL MCC Model has been trained 
+and saved in the following file:
+  %1", dl.basic.model.file_path)
+
+  put_log("Saving the BDL MCC Model History...")
+  saveRDS(dl.basic.train.flatten_history,
+          file = dl.basic.model.train_history.file_path)
+  
+  put_log("The BDL MCC Model History has been trained 
+and saved in the following file:
+  %1", dl.basic.model.train_history.file_path)
+  put_end_date(start)
+  # Time difference of 38.48235 mins
+}
+
+put_log("The Basic `DL MCC` Model has been trained with the following results
+%1", dl.basic.model)
+
+
+plot(dl.basic.train.flatten_history)
+str(dl.basic.train.flatten_history)
+
+log_close()
+
+## BDL MCC Model Evaluation ----------------------------------------------------
+open_logfile(".dl.basic-model.evaluate")
+
+put_log("Evaluating DL Model...")
+bdl.eval.result <- dl.basic.model |> evaluate(x_test, y_test.cat)
+put_log("DL Model evaluation result:
+%1", capture.output(str(bdl.eval.result)))
+# List of 2
+#  $ accuracy: num 0.897
+#  $ loss    : num 0.314
+
+put_end_date(start)
+# Time difference of 1.668308 mins
+
+bdl.preds <- dl.basic.model |> predict(x_test) 
+put_end_date(start)
+# Time difference of  mins
+
+colnames(bdl.preds) <- Y.Labels
+head(bdl.preds[,1:5])
+#                 #            $            &            @            0
+# [1,] 1.291058e-08 2.551113e-09 1.649115e-10 3.021582e-07 1.282524e-06
+# [2,] 1.855945e-11 2.930238e-11 1.776997e-09 3.246364e-06 1.664781e-08
+# [3,] 2.074071e-11 1.434007e-10 3.564378e-09 8.688334e-12 2.087918e-05
+# [4,] 1.167588e-09 1.428053e-10 3.141675e-11 4.189771e-10 1.695369e-07
+# [5,] 9.645254e-13 3.239760e-12 2.698784e-14 9.448751e-12 2.331821e-09
+# [6,] 2.562272e-10 2.353311e-13 3.094632e-13 2.608500e-11 4.482589e-08
+dim(bdl.preds)
+#> [1] 33228    39
+
+bdl.preds.ts <- as_tensor(bdl.preds)
+str(bdl.preds.ts)
+#> <tf.Tensor: shape=(684467, 39), dtype=float64, numpy=…>
+
+bdl.predictions <- bdl.preds.ts |> op_argmax(2)
+bdl.predictions
+#> tf.Tensor([13  4 21 ... 19  5  1], shape=(684467), dtype=int32)
+dim(bdl.predictions)
+#> [1] 33228
+# bdl.predictions$numpy()
+
+
+# y_test
+# as.integer(y_test)
+
+bdl.pred.values.idx <- bdl.predictions$numpy()
+head(bdl.pred.values.idx)
+
+bdl.pred.values <- Y.Labels[bdl.pred.values.idx]
+head(bdl.pred.values)
+
+dl.basic.accuracy <- mean(bdl.pred.values.idx == as.integer(y_test))
+put_log("The overall Basic `DL MCC` Model accuracy: %1",dl.basic.accuracy)
+# 0.897195136631756
+
+log_close()
+
+## Visualizing the Evaluation Results ------------------------------------------
+
+open_logfile(".rf-tuned.eval-results.visualization")
+
+stopifnot(file.exists(model_visualization.shared.script.path))
+
+rf_tuned.eval.conf.mx.img_file <- file.path(data.models.rf.plots.dat.dir,
+                                            "rf-tuned.eval.confusion-matrix.png")
+
+rf_tuned.eval.plots_dat.file <- file.path(data.models.rf.plots.dat.dir,
+                                          "rf-tuned.eval.plots_dat.rds")
+
+#' Initialize the `plots.args` object containing argument values 
+#' for the visualization helper functions being called in the following script 
+#' about to launch:
+if(file.exists(rf_tuned.eval.plots_dat.file)) {
+  put_log("Function `init.plots_args`:
+Loading the model-related plots input data object from the backup file...")
+  plots.args <- init.plots_args(rf_tuned.eval.plots_dat.file)
+  
+  put_log("Function `init.plots_args`:
+The model-related plots input data object has been loaded from the following file:
+%1", rf_tuned.eval.plots_dat.file)
+} else {
+  plots.args <- init.plots_args(targets = fit_rf.mtry_best$test$targets,
+                                predicted.probabilities = fit_rf.mtry_best$test$votes,
+                                predicted.values = fit_rf.mtry_best$test$predicted,
+                                alg_name = "Random Forest",
+                                plots_dat.file = rf_tuned.eval.plots_dat.file,
+                                cm.export.img_file = rf_tuned.eval.conf.mx.img_file,
+                                cm.print.image = T)
+}
+
+#'Run the helper script specifically designed to visualize 
+#'the model evaluation results:
+source(model_visualization.shared.script.path,
+       catch.aborts = TRUE,
+       echo = TRUE,
+       spaced = TRUE,
+       verbose = TRUE,
+       keep.source = TRUE)
+
+rm(plots.args,
+   fit_rf.mtry_best)
+
+stopifnot(exists("plots.dat"),
+          !is.null(plots.dat$ROC),
+          !is.null(plots.dat$PCA),
+          !is.null(plots.dat$CM))
+
+put_log("Saving the model-related plots input data object to file...")
+
+saveRDS(plots.dat,
+        file = rf_tuned.eval.plots_dat.file)
+
+put_log("The model-related plots input data object has been saved to the following file:
+%1", rf_tuned.eval.plots_dat.file)
+
+rm(plots.dat)
+log_close()
+
+
+## Evaluation Results: Visualization -------------------------------------------
+open_logfile(".dl.basic-model.build")
+
+start <- put_start_date()
+
+put_log("Plotting ROC curves the Model Evaluation Results...")
+dl.basic.roc_curves <- plot.ROC.curves(y_test,
+                                       bdl.preds)
+Sys.sleep(6)
+
+put_log("Plotting the Tuned BDL MCC Model Per-Class Accuracy...")
+dl.basic.accuracy.by_class <- plot.per_class.accuracy.bars(y_test,
+                                                           bdl.pred.values)
+
+put_log("The following values of the BDL MCC Model Per-Class Accuracy have been plotted:
+%1", capture.output(dl.basic.accuracy.by_class))
+{
+  
+  invisible(NULL)
+}
+
+Sys.sleep(5)
+
+# Confusion Matrix data suitable for Visualization using the `cvms` package:
+# Reference: https://cran.r-project.org/web/packages/cvms/vignettes/Creating_a_confusion_matrix.html
+
+put_log("Creating a confusion matrix for Tuned BDL MCC Model in a format suitable for visualization 
+using the `cvms` package...")
+
+dl.basic.conf.mx <- create.confusion_matrix(y_test,
+                                                          bdl.pred.values)
+
+put_log("The confusion matrix based on the `BDL MCC` Model evaluation results has been created:
+%1", capture.output(dl.basic.conf.mx))
+
+put_log("Plotting the confusion matrix, please wait...")
+dl.basic.conf.mx.chart <- plot.conf.mx(dl.basic.conf.mx)
+
+start <- put_start_date()
+cl <- makeCluster(N_pcCores)
+registerDoParallel(cl)
+
+dev.off()
+print(dl.basic.conf.mx.chart)
+
+stopCluster(cl)
+stopImplicitCluster()
+
+put_end_date(start)
+log_close()
